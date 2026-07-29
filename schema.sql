322a85052c979d88edd4fb77e26c32dd9ec32e7a
AlfredKa <alfredmash01@gmail.com>
2026-07-29 11:27:20 +0300

    Updated schem.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,12 @@
-CREATE TABLE catastrophe
+CREATE TABLE catastrophe (
+
+    event_id TEXT PRIMARY KEY,
+    event_time TIMESTAMP,
+    magnitude NUMERIC,
+    depth_km NUMERIC,
+    latitude NUMERIC,
+    longitude NUMERIC,
+    place TEXT,
+    risk_tier TEXT
+
+);
